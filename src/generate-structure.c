97a58a49e938b86ff7577de31b51f1fcbf9912b9
podlaskialgorytm <live.m851@gmail.com>
2024-12-09 20:19:53 +0100

    feat: change y to x

--- src/generate-structure.c
+++ src/generate-structure.c
@@ -7,12 +7,12 @@ void getRandomTemperatures(temperaturePointer *list, double min, double max, int
 {
     srand(time(NULL));
 
-    for (int i = 1; i < x + 1; i++)
+    for (int i = 1; i < y + 1; i++)
     {
-        for (int j = 1; j < y + 1; j++)
+        for (int j = 1; j < x + 1; j++)
         {
             double value = min + (double)rand() / RAND_MAX * (max - min);
-            *list = insert(*list, i, j, value);
+            *list = insert(*list, j, i, value);
         }
     }
 }
