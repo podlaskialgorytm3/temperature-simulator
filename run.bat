d37e3489dc766438b44938a71ea53aa0bde5daa7
podlaskialgorytm <live.m851@gmail.com>
2024-12-17 00:23:03 +0100

    feat: give except to new file with converting

--- run.bat
+++ run.bat
@@ -9,9 +9,9 @@ gcc -c src\generate-structure.c -o build\object-files\generate-structure.o
 gcc -c src\io.c -o build\object-files\io.o
 gcc -c src\set-temperature.c -o build\object-files\set-temperature.o
 gcc -c src\get-temperature.c -o build\object-files\get-temperature.o
+gcc -c src\converting.c -o build\object-files\converting.o
 
 rem Link object files into an executable
-gcc build\object-files\main.o build\object-files\temperature.o build\object-files\generate-structure.o build\object-files\io.o build\object-files\set-temperature.o build\object-files\get-temperature.o -o program.exe
-
+gcc build\object-files\main.o build\object-files\temperature.o build\object-files\generate-structure.o build\object-files\io.o build\object-files\set-temperature.o build\object-files\get-temperature.o build\object-files\converting.o -o program.exe
 
 rem End of script
